46ecefba406b8b1775d147d53acebb31328fa83a
Hanka Milotova <hanka.esha2@gmail.com>
2022-03-15 12:08:53 +0100

    Playground sizes option added, language change on new game button

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
 console.clear();
 
-const playgroundSideCount = 6;
+const gameVariants = {
+  small: {
+    className: "container-small",
+    playgroundSideCount: 2,
+    subtitle: "Hrajete hru pro malé nebo začínající hráče",
+  },
+  medium: {
+    className: "container-medium",
+    playgroundSideCount: 4,
+    subtitle: "Hrajete hru pro odvážnější hráče",
+  },
+  large: {
+    className: "container-large",
+    playgroundSideCount: 8,
+    subtitle: "Hrajete hru pro pexeso profíky",
+  },
+};
+
+let gameVariant = "small";
+
+const playgroundSideCount = gameVariants[gameVariant].playgroundSideCount;
+
+document
+  .getElementById("playground")
+  .classList.add(gameVariants[gameVariant].className);
+
+document.getElementById("subtitle").innerHTML =
+  gameVariants[gameVariant].subtitle;
+
 let cardsTurned = [];
 
-const play = (size) => {
-  document.getElementById("playground").innerHTML = "";
-  let numbersList = [];
-  for (let i = 0; i < size; i++) {
-    numbersList.push(0);
-  }
-  for (let i = 0; i < 2*size; i++) {
-    let div = document.createElement("div");
-    document.getElementById("playground").appendChild(div).classList.add("card");
-  }
-  console.log(numbersList);
-
-  let cards = document.querySelectorAll(".card");
-  for (let card of cards) {
-    let number = Math.floor(Math.random()*size);
-    while (numbersList[number] === 2){
-      number = Math.floor(Math.random()*size);
-    }
-    numbersList[number]++;
-    card.innerHTML = number +1;
-  }
-  console.log(numbersList);
-  return cards
-};
-
-let cards = play(playgroundSideCount*playgroundSideCount/2);
-
-const turnCardsBack = () => {
-  console.log("obratit karty", cardsTurned);
-  cardsTurned.forEach((card) => {
-    card.classList.remove("card-turned");
-  })
-  cardsTurned = [];
-};
-
-const gameOver = () => {
-  if (document.querySelectorAll(".card-out").length === playgroundSideCount*playgroundSideCount) {
-    document.getElementById("message").classList.remove("message-hidden");
-    console.log("You won!");
-  }
-};
-
 const game = (event) => {
-  // console.log(event.target.classList.contains("card-turned"));
-  // console.log(event.target.classList.contains("card-out"));
-  if (!event.target.classList.contains("card-turned") && !event.target.classList.contains("card-out")) {
+  if (
+    !event.target.classList.contains("card-turned") &&
+    !event.target.classList.contains("card-out")
+  ) {
     cardsTurned.push(event.target);
     event.target.classList.add("card-turned");
     console.log("if/cardTurned", event.target.classList);
@@ -59,17 +46,72 @@ const game = (event) => {
           cardsTurned[i].classList.remove("card-turned");
           cardsTurned[i].classList.add("card-out");
           console.log("cyklus pro zmenu na card-out", event.target.classList);
-        } 
+        }
         cardsTurned = [];
       } else {
-          setTimeout(turnCardsBack, 1000);
+        setTimeout(turnCardsBack, 1000);
       }
     }
-  } 
+  }
   gameOver();
 };
 
-for (let card of cards) {
-    console.log("hlavni cyklus", card.classList);
-    card.addEventListener("click", game)
+const generatePlayground = (size) => {
+  document.getElementById("message").classList.add("message-hidden");
+  document.getElementById("playground").innerHTML = "";
+  let numbersList = [];
+  for (let i = 0; i < size; i++) {
+    numbersList.push(0);
   }
+  for (let i = 0; i < 2 * size; i++) {
+    let div = document.createElement("div");
+    document
+      .getElementById("playground")
+      .appendChild(div)
+      .classList.add("card");
+  }
+  console.log(numbersList);
+
+  let cards = document.querySelectorAll(".card");
+  for (let card of cards) {
+    let number = Math.floor(Math.random() * size);
+    while (numbersList[number] === 2) {
+      number = Math.floor(Math.random() * size);
+    }
+    numbersList[number]++;
+    card.innerHTML = number + 1;
+  }
+  console.log(numbersList);
+
+  for (let card of cards) {
+    console.log("hlavni cyklus", card.classList);
+    card.addEventListener("click", game);
+  }
+};
+
+const play = () => {
+  generatePlayground((playgroundSideCount * playgroundSideCount) / 2);
+};
+
+play();
+
+let playButton = document.getElementById("play-reset");
+playButton.addEventListener("click", play);
+
+const turnCardsBack = () => {
+  console.log("obratit karty", cardsTurned);
+  cardsTurned.forEach((card) => {
+    card.classList.remove("card-turned");
+  });
+  cardsTurned = [];
+};
+
+const gameOver = () => {
+  if (
+    document.querySelectorAll(".card-out").length ===
+    playgroundSideCount * playgroundSideCount
+  ) {
+    document.getElementById("message").classList.remove("message-hidden");
+    console.log("Výhra!");
+  }
+};
