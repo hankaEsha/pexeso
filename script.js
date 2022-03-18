98d3736c0ce484c9b94fbc800e280f43a3305ea5
Hanka Milotova <hanka.esha2@gmail.com>
2022-03-18 12:12:50 +0100

    3 buttons changing the game level added in JS

--- script.js
+++ script.js
@@ -5,42 +5,25 @@ const gameVariants = {
     className: "container-small",
     playgroundSideCount: 2,
     subtitle: "Hrajete hru pro malé nebo začínající hráče",
+    buttonText: "Nová malá hra",
   },
   medium: {
     className: "container-medium",
     playgroundSideCount: 4,
     subtitle: "Hrajete hru pro odvážnější hráče",
+    buttonText: "Nová střední hra",
   },
   large: {
     className: "container-large",
     playgroundSideCount: 8,
     subtitle: "Hrajete hru pro pexeso profíky",
+    buttonText: "Nová velká hra",
   },
 };
 
-let gameVariant = "large";
-
-const playgroundSideCount = gameVariants[gameVariant].playgroundSideCount;
-
-document
-  .getElementById("playground")
-  .classList.add(gameVariants[gameVariant].className);
-
-document.getElementById("subtitle").innerHTML =
-  gameVariants[gameVariant].subtitle;
-
-
-// Create a new, plain <button> element
-let playAgainButton = document.createElement("button");
-// Get the reference child element
-let winMessage = document.getElementById("message");
-
-// Get the parent element and insert the playAgainButton before the winMessage node
-winMessage.parentNode.insertBefore(playAgainButton, winMessage);
-playAgainButton.classList.add("play-reset");
-playAgainButton.innerHTML = "Nová hra";
-
 let cardsTurned = [];
+let gameVariant = gameVariants.medium;
+const playground = document.getElementById("playground");
 
 const game = (event) => {
   if (
@@ -49,14 +32,11 @@ const game = (event) => {
   ) {
     cardsTurned.push(event.target);
     event.target.classList.add("card-turned");
-    console.log("if/cardTurned", event.target.classList);
     if (cardsTurned.length === 2) {
       if (cardsTurned[0].innerHTML === cardsTurned[1].innerHTML) {
-        console.log("stalo se");
         for (let i = 0; i < cardsTurned.length; i++) {
           cardsTurned[i].classList.remove("card-turned");
           cardsTurned[i].classList.add("card-out");
-          console.log("cyklus pro zmenu na card-out", event.target.classList);
         }
         cardsTurned = [];
       } else {
@@ -69,19 +49,15 @@ const game = (event) => {
 
 const generatePlayground = (size) => {
   document.getElementById("message").classList.add("message-hidden");
-  document.getElementById("playground").innerHTML = "";
+  playground.innerHTML = "";
   let numbersList = [];
   for (let i = 0; i < size; i++) {
     numbersList.push(0);
   }
   for (let i = 0; i < 2 * size; i++) {
     let div = document.createElement("div");
-    document
-      .getElementById("playground")
-      .appendChild(div)
-      .classList.add("card");
+    playground.appendChild(div).classList.add("card");
   }
-  console.log(numbersList);
 
   let cards = document.querySelectorAll(".card");
   for (let card of cards) {
@@ -92,24 +68,43 @@ const generatePlayground = (size) => {
     numbersList[number]++;
     card.innerHTML = number + 1;
   }
-  console.log(numbersList);
 
   for (let card of cards) {
-    console.log("hlavni cyklus", card.classList);
     card.addEventListener("click", game);
   }
 };
 
-const play = () => {
+const play = (event) => {
+  if (event) {
+    gameVariant = gameVariants[event.target.dataset.gameVariant];
+  }
+  let playgroundSideCount = gameVariant.playgroundSideCount;
+  playground.classList.remove(
+    "container-small",
+    "container-medium",
+    "container-large"
+  );
+  playground.classList.add(gameVariant.className);
+  document.getElementById("subtitle").innerHTML = gameVariant.subtitle;
   generatePlayground((playgroundSideCount * playgroundSideCount) / 2);
 };
 
+for (const variant in gameVariants) {
+  // Create a new, plain <button> element
+  let gameVariantButton = document.createElement("button");
+  gameVariantButton.innerHTML = gameVariants[variant].buttonText;
+  gameVariantButton.classList.add("play-reset");
+  gameVariantButton.dataset.gameVariant = variant;
+  gameVariantButton.addEventListener("click", play);
+  // Get the reference child element
+  let winMessage = document.getElementById("message");
+  // Get the parent element and insert the button element before the referenced child element
+  winMessage.parentNode.insertBefore(gameVariantButton, winMessage);
+}
+
 play();
 
-playAgainButton.addEventListener("click", play);
-
 const turnCardsBack = () => {
-  console.log("obratit karty", cardsTurned);
   cardsTurned.forEach((card) => {
     card.classList.remove("card-turned");
   });
@@ -117,11 +112,11 @@ const turnCardsBack = () => {
 };
 
 const gameOver = () => {
+  let playgroundSideCount = gameVariant.playgroundSideCount;
   if (
     document.querySelectorAll(".card-out").length ===
     playgroundSideCount * playgroundSideCount
   ) {
     document.getElementById("message").classList.remove("message-hidden");
-    console.log("Výhra!");
   }
 };
